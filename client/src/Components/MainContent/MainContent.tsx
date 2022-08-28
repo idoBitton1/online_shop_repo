e16013bf9c1537cf8d07a1c9e64efd57cb8e1ccf
idoBitton1 <idob5964@gmail.com>
2022-08-28 21:55:46 +0300

    UI changes

--- client/src/Components/MainContent/MainContent.tsx
+++ client/src/Components/MainContent/MainContent.tsx
@@ -14,11 +14,70 @@ interface MyProps{
     salary_per_hour: number
 }
 
+const getMonthName = (month_number: number): string => {
+
+    var month_name: string = "";
+    switch (month_number) {
+        case 1:
+            month_name = "January";
+            break;
+        case 2:
+            month_name = "February";
+            break;
+        case 3:
+            month_name = "March";
+            break;
+        case 4:
+            month_name = "April";
+            break;
+        case 5:
+            month_name = "May";
+            break;
+        case 6:
+            month_name = "June";
+            break;
+        case 7:
+            month_name = "July";
+            break;
+        case 8:
+            month_name = "August";
+            break;
+        case 9:
+            month_name = "September";
+            break;
+        case 10:
+            month_name = "October";
+            break;
+        case 11:
+            month_name = "November";
+            break;
+        case 12:
+            month_name = "December";
+            break;
+    }
+    return month_name;
+}
+
 export const MainContent: React.FC<MyProps> = ({records, special_records, extras}) => {
+
+    var current_date: Date = new Date();
+    current_date.setMonth(current_date.getMonth() + 1);
 
     return(
         <>
         <div className="main_content_container">    
+
+            <Grid container sx={{textAlign: "center"}}>
+                <Grid item xs={2}>
+                    left
+                </Grid>
+                <Grid item xs={8}>
+                    {getMonthName(current_date.getMonth())}
+                </Grid>
+                <Grid item xs={2}>
+                    right
+                </Grid>
+            </Grid>
 
             <Grid container sx={{
                     backgroundColor: "#30a890",
@@ -29,6 +88,7 @@ export const MainContent: React.FC<MyProps> = ({records, special_records, extras
                     paddingRight: "2em",
                     textAlign: "center"
                 }}>
+
                 <Grid item xs={2}>
                     date
                 </Grid>
