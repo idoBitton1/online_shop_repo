e5967982fcc383ab78a40dbe5b4cbc470b41070a
idoBitton1 <idob5964@gmail.com>
2022-09-11 13:19:13 +0300

    updated the createUser mutation

--- client/src/Queries/Mutations.tsx
+++ client/src/Queries/Mutations.tsx
@@ -2,8 +2,8 @@ import React from "react"
 import { gql } from "@apollo/client"
 
 export const MUTATION_CREATE_USER = gql`
-  mutation Mutation($username: String!, $password: String!) {
-    createUser(username: $username, password: $password) {
+  mutation Mutation($username: String!, $password: String!, $confirmPassword: String!) {
+    createUser(username: $username, password: $password, confirm_password: $confirmPassword) {
       id
     }
   }
