946fd1ae1b87325e854e1204e4d8964fa45e2f8d
idoBitton1 <idob5964@gmail.com>
2022-09-15 21:04:08 +0300

    deleted the js server

--- server/index.js
+++
@@ -1,9 +0,0 @@
-const { ApolloServer } = require("apollo-server");
-const { typeDefs } = require("./schema/typeDefs");
-const { resolvers } = require("./schema/resolvers"); 
-
-const server = new ApolloServer({typeDefs, resolvers});
-
-server.listen().then(({ url }) => {
-    console.log(`API is running at: ${url}`);
-})
