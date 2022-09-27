725c37d4b4eed76331aadfed5450e291162abbe2
idoBitton1 <idob5964@gmail.com>
2022-09-27 12:46:25 +0300

    moved all the adding components because i dont use them in the footer anymore

--- client/src/Components/Footer/Footer.tsx
+++ client/src/Components/Footer/Footer.tsx
@@ -1,33 +1,15 @@
 import React from "react"
 import "./Footer.css"
 
-//Components
-import { AddExtra } from "./AddExtra"
-import { AddRecord } from "./AddRecord"
-import { AddSpecialRecord } from "./AddSpecialRecord"
-
 export interface MyProps{
 
-  user_id: string,
-  job_id: string,
 }
 
-export const Footer: React.FC<MyProps> = ({user_id, job_id}) => {
+export const Footer: React.FC<MyProps> = () => {
 
   return(
     <footer>
-      <AddRecord 
-        user_id={user_id} 
-        job_id={job_id}
-      />
-      <AddExtra
-        user_id={user_id} 
-        job_id={job_id} 
-      />
-      <AddSpecialRecord
-        user_id={user_id} 
-        job_id={job_id}
-      />
+      
     </footer>
   )
 }
