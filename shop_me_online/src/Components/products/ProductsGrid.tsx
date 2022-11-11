013cb8d4a4352b522a93059e102d66bb006572da
idoBitton1 <idob5964@gmail.com>
2022-11-11 14:57:06 +0200

    removed the categories bar

--- shop_me_online/src/Components/products/ProductsGrid.tsx
+++ shop_me_online/src/Components/products/ProductsGrid.tsx
@@ -3,13 +3,11 @@ import "./Products.css"
 
 //components
 import { ProductDisplay } from "./ProductDisplay";
-import { CatergoriesBar } from "./CategoriesBar";
 
 export const ProductsGrid = () => {
 
     return(
         <>
-        <CatergoriesBar />
         <div className="products_grid">
             <ProductDisplay />
             <ProductDisplay />
