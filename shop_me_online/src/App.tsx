8374fc0a8440c192cda1fade7ad6b80444d29f4d
idoBitton1 <idob5964@gmail.com>
2022-11-11 14:57:36 +0200

    added the categories bar to the screen

--- shop_me_online/src/App.tsx
+++ shop_me_online/src/App.tsx
@@ -3,12 +3,14 @@ import './App.css';
 
 //components
 import { Header } from './Components/Header/Header';
+import { CatergoriesBar } from './Components/products/CategoriesBar';
 import { ProductsGrid } from './Components/products/ProductsGrid';
 
 function App() {
   return (
     <div className="app_container">
       <Header />
+      <CatergoriesBar />
       <ProductsGrid />
     </div>
   );
