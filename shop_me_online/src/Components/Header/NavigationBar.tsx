e4f6ed3888c6aeca286eb0683440d09eee50ea59
idoBitton1 <idob5964@gmail.com>
2022-11-13 13:53:28 +0200

    added filters

--- shop_me_online/src/Components/Header/NavigationBar.tsx
+++ shop_me_online/src/Components/Header/NavigationBar.tsx
@@ -1,9 +1,10 @@
-import React from "react";
+import React, {useState} from "react";
 
 //material ui
-import { Button } from "@mui/material";
+import { Button, FormControl, InputLabel, MenuItem, Select, SelectChangeEvent } from "@mui/material";
 
 //icons
+import CircleIcon from '@mui/icons-material/Circle';
 import {GiConverseShoe} from 'react-icons/gi';
 import {FaTshirt} from 'react-icons/fa';
 import {GiShorts} from 'react-icons/gi';
@@ -18,7 +19,37 @@ interface CategoryStyle{
     onClickFunc?: () => void //delete ? later
 }
 
+interface Filters{
+    color: string,
+    season: string
+}
+
+const chooseCurrentSeason = ():string => {
+    const current_date = new Date();
+
+    const current_month = current_date.getMonth() + 1;
+
+    if(current_month === 12 || current_month === 1 || current_month === 2)
+        return "winter";
+    else if(current_month >= 3 && current_month <= 5)
+        return "spring";
+    else if(current_month >= 6 && current_month <= 8)
+        return "summer";
+    else
+        return "autumn"
+}
+
 export const CatergoriesBar = () => {
+
+    const [filters, setFilters] = useState<Filters>({
+        color: "any",
+        season: chooseCurrentSeason()
+    });
+
+    //colors array
+    const colors_array: string[] = [
+        "any", "black", "blue", "green"
+    ]
 
     //categories array
     const categories_array: CategoryStyle[] = [
@@ -62,17 +93,77 @@ export const CatergoriesBar = () => {
             return 0;
     })
 
+    const handleColorChange = (event: SelectChangeEvent) => {
+        setFilters((prev) => {
+            return {
+                color: event.target.value as string,
+                season: prev.season
+            }
+        })
+    }
+
+    const handleSeasonChange = (event: SelectChangeEvent) => {
+        setFilters((prev) => {
+            return {
+                color: prev.color,
+                season: event.target.value as string
+            }
+        })
+    }
+
     return(
-        <div className="categories_container">
-            {categories_array.map((item) => {
+        <div className="navigation_bar">
+            <div>
+            {categories_array.map((item, i) => {
                 return(
-                    <Button className="category_style" 
+                    <Button key={i} className="category_style" 
                     sx={{textTransform: "none", color: "gray", marginLeft: 3, fontFamily: "Rubik"}}>
                         {item.icon}
                         {item.category_name} 
                     </Button>
                 )
             })}
+            </div>
+
+            <div>
+            <FormControl variant="standard" sx={{width: 150, marginRight: 3}}>
+                <InputLabel>Color</InputLabel>
+                <Select
+                id="color_select"
+                value={filters.color}
+                label="Color"
+                onChange={handleColorChange}
+                >
+                {
+                    colors_array.map((colorI, i) => {
+                        if(colorI === "any")
+                            return(
+                                <MenuItem key={i} value={"any"}>any</MenuItem>
+                            )
+                        else
+                            return (
+                                <MenuItem key={i} value={colorI}>{colorI}{<CircleIcon sx={{marginLeft: "auto", color: colorI, marginBottom: -1}} />}</MenuItem>
+                            )
+                    })
+                }
+                </Select>
+            </FormControl>
+
+            <FormControl variant="standard" sx={{width: 120}}>
+                <InputLabel>Season</InputLabel>
+                <Select
+                id="season_select"
+                value={filters.season}
+                label="Season"
+                onChange={handleSeasonChange}
+                >
+                <MenuItem value={"spring"}>spring</MenuItem>
+                <MenuItem value={"summer"}>summer</MenuItem>
+                <MenuItem value={"autumn"}>autumn</MenuItem>
+                <MenuItem value={"winter"}>winter</MenuItem>
+                </Select>
+            </FormControl>
+            </div>
         </div>
     )
 }
