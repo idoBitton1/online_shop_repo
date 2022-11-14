e03feb678f0e01aec1fc550b09df1a3f5243b530
idoBitton1 <idob5964@gmail.com>
2022-11-14 22:01:39 +0200

    added sample data

--- shop_me_online/src/App.tsx
+++ shop_me_online/src/App.tsx
@@ -1,17 +1,45 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
 //components
 import { Header } from './Components/Header/Header';
-import { CatergoriesBar } from './Components/Header/NavigationBar';
+import { NavigationBar } from './Components/Header/NavigationBar';
 import { ProductsGrid } from './Components/products/ProductsGrid';
 
+export interface Product{
+  id: string,
+  name: string,
+  price: number,
+  quantity: number,
+  categories: string
+}
+
 function App() {
+
+  const [products, setProducts] = useState<Product[]>([
+    {
+      id: "1",
+      name: "air jordan 1 lucky green",
+      price: 150,
+      quantity: 150,
+      categories: "#shoes#green"
+    },
+    {
+      id: "2",
+      name: "air jordan travis scott",
+      price: 300,
+      quantity: 100,
+      categories: "#shoes#black"
+    }
+  ]);
+
+  const [filtered_products, setFilteredProducts] = useState<Product[]>(products);
+
   return (
     <div className="app_container">
       <Header />
-      <CatergoriesBar />
-      <ProductsGrid />
+      <NavigationBar products={products} filtered_products={filtered_products} setFilteredProducts={setFilteredProducts} />
+      <ProductsGrid products={products} filtered_products={filtered_products} />
     </div>
   );
 }
