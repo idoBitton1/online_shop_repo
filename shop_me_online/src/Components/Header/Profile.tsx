a92571425b97a6f5c1f73d523c8c4be248bf6456
idoBitton1 <idob5964@gmail.com>
2022-11-14 22:03:39 +0200

    added a dropdown list on click

--- shop_me_online/src/Components/Header/Profile.tsx
+++ shop_me_online/src/Components/Header/Profile.tsx
@@ -1,4 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
+
+//components
+import { DropdownItem } from "./DropdownItem";
 
 //material ui icons
 import MenuIcon from '@mui/icons-material/Menu';
@@ -6,12 +9,28 @@ import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 
 export const Profile = () => {
 
+    const [open, setOpen] = useState<boolean>(false);
+
+    const handleClick = () => {
+        setOpen((prev) => !prev)
+    }
+
     return(
         <>
-        <button className="button profile_btn">
+        <button className="button profile_btn" onClick={handleClick}>
             <MenuIcon sx={{fontSize: 40}} />
             <AccountCircleIcon sx={{fontSize: 40}} />
         </button>
+
+        <div className={`dropdown-menu ${open? 'active' : 'inactive'}`} >
+          <ul>
+            <DropdownItem />
+            <DropdownItem />
+            <DropdownItem />
+            <DropdownItem />
+            <DropdownItem />
+          </ul>
+        </div>
         </>
     )
 }
