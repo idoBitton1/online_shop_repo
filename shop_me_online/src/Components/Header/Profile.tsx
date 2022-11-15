32e80ce9983642cfe8c3b6662ddcea1a7420faec
idoBitton1 <idob5964@gmail.com>
2022-11-15 12:31:29 +0200

    fixed dropdown list

--- shop_me_online/src/Components/Header/Profile.tsx
+++ shop_me_online/src/Components/Header/Profile.tsx
@@ -3,9 +3,12 @@ import React, { useState } from "react";
 //components
 import { DropdownItem } from "./DropdownItem";
 
-//material ui icons
+//icons
 import MenuIcon from '@mui/icons-material/Menu';
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import {CiLogin} from 'react-icons/ci';
+import {CiLogout} from 'react-icons/ci';
+import {FiUserPlus} from 'react-icons/fi';
 
 export const Profile = () => {
 
@@ -22,13 +25,28 @@ export const Profile = () => {
             <AccountCircleIcon sx={{fontSize: 40}} />
         </button>
 
-        <div className={`dropdown-menu ${open? 'active' : 'inactive'}`} >
+        <div className={`dropdown_menu ${open ? 'active' : 'inactive'}`} >
           <ul>
-            <DropdownItem />
-            <DropdownItem />
-            <DropdownItem />
-            <DropdownItem />
-            <DropdownItem />
+            <li className = 'dropdown_item'>
+              <FiUserPlus className="dropdown_item_icon"/>
+              <a style={{fontWeight: "bold"}}> sign up </a>
+            </li>
+            <li className = 'dropdown_item'>
+              <CiLogin className="dropdown_item_icon"/>
+              <a> log in </a>
+            </li>            
+            <li className = 'dropdown_item'>
+              <FiUserPlus className="dropdown_item_icon"/>
+              <a> become a manager </a>
+            </li>
+            <li className = 'dropdown_item'>
+              <CiLogin className="dropdown_item_icon"/>
+              <a> manager log in </a>
+            </li>            
+            <li className = 'dropdown_item'>
+              <CiLogout className="dropdown_item_icon"/>
+              <a> log out </a>
+            </li>
           </ul>
         </div>
         </>
