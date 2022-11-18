85368229e717ec50c319f43d5db6a0f3b04b59bc
idoBitton1 <idob5964@gmail.com>
2022-11-18 15:58:50 +0200

    added cart state to redux

---
+++ shop_me_online/src/state/reducers/cartReducer.ts
@@ -0,0 +1,25 @@
+import { Cart } from "../../App";
+import { CartActions } from "../actions";
+import { CartActionType } from "../action_types"
+
+const initial_state: Cart[] = [];
+
+const reducer = (state: Cart[] = initial_state, action: CartActions) => {
+    switch(action.type)
+    {
+        case CartActionType.ADD_PRODUCT:
+            return [...state, action.payload];
+        case CartActionType.REMOVE:
+            {
+                let index_of_current_product = -1;
+
+                index_of_current_product = state.findIndex((product) => product.product_id === action.payload);
+
+                return [...state.slice(0,index_of_current_product), ...state.slice(index_of_current_product + 1)];
+            }
+        default:
+            return state;
+    }
+}
+
+export default reducer;
