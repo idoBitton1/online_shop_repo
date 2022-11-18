3a8fac3df3f09bdae6699d41814f196884ea59e2
idoBitton1 <idob5964@gmail.com>
2022-11-18 13:36:45 +0200

    added redux-provider

--- shop_me_online/src/index.tsx
+++ shop_me_online/src/index.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { Provider } from 'react-redux';
+import { store } from './state/index';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -9,6 +11,8 @@ const root = ReactDOM.createRoot(
 
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
