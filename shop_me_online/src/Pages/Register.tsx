f84b45843add165604892509f407f24b0aa0ad18
idoBitton1 <idob5964@gmail.com>
2022-11-28 19:54:03 +0200

    created a new page

---
+++ shop_me_online/src/Pages/Register.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import * as Yup from "yup"
+import { Formik, Form, Field, ErrorMessage } from "formik"
+
+//components
+import { Header } from '../Components/Header/Header';
+
+const Register = () => {
+
+    return(
+        <div className="register_container">
+            <Header />
+
+
+        </div>
+    );
+}
+
+export default Register;
