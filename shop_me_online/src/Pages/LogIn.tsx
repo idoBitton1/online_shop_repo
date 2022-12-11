2ebf08f62cb9f2aa8c6ee5c5f297bf4475b55190
idoBitton1 <idob5964@gmail.com>
2022-12-11 13:53:58 +0200

    the login page

---
+++ shop_me_online/src/Pages/LogIn.tsx
@@ -0,0 +1,22 @@
+import React from "react"
+import './LogIn.css';
+
+//components
+import { Header } from '../Components/Header/Header';
+import { LogInForm } from "../Components/Forms/LogInForm";
+
+const LogIn = () => {
+
+    return (
+        <div className="login_container">
+            <Header />
+
+            <div className="login_form">
+                <h1 style={{fontFamily: "Arial"}}> Welcome back </h1> 
+                <LogInForm />
+            </div>
+        </div>
+    );
+}
+
+export default LogIn;
