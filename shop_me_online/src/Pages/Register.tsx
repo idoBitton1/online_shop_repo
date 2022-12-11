a72af7d6216a48f6b7640434ae0a862a69f0f01e
idoBitton1 <idob5964@gmail.com>
2022-12-11 12:22:18 +0200

    added a prop to know if a menager is registering

--- shop_me_online/src/Pages/Register.tsx
+++ shop_me_online/src/Pages/Register.tsx
@@ -5,14 +5,19 @@ import './Register.css';
 import { Header } from '../Components/Header/Header';
 import { RegisterForm } from '../Components/Forms/RegisterForm';
 
-const Register = () => {
+interface MyProps{
+    is_manager: boolean
+}
+
+const Register: React.FC<MyProps> = ({is_manager}) => {
 
     return(
         <div className="register_container">
             <Header />
 
             <div className="register_form">
-                <RegisterForm />
+                <h1 style={{fontFamily: "Arial"}}>{is_manager ? "Become a manager" : "Create an account"}</h1>            
+                <RegisterForm is_manager={is_manager} />           
             </div>
         </div>
     );
