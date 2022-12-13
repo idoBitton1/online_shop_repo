916da41afc4c1e541fb4a50b8fd4be4f1ced0881
idoBitton1 <idob5964@gmail.com>
2022-12-13 15:14:33 +0200

    now also fetching the token field when creating a user and logging in

--- shop_me_online/src/Queries/Mutations.tsx
+++ shop_me_online/src/Queries/Mutations.tsx
@@ -4,6 +4,7 @@ export const CREATE_USER = gql`
 mutation Mutation($firstName: String!, $lastName: String!, $password: String!, $address: String!, $email: String!, $isManager: Boolean!) {
     createUser(first_name: $firstName, last_name: $lastName, password: $password, address: $address, email: $email, is_manager: $isManager) {
       id
+      token
     }
   }
 `;
@@ -12,6 +13,7 @@ export const LOGIN_USER = gql`
 mutation Mutation($email: String!, $password: String!) {
     loginUser(email: $email, password: $password) {
       id
+      token
     }
   }
 `;
