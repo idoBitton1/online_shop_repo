a77932bf7e558ebe66e5e41edf818b5f079475e9
idoBitton1 <idob5964@gmail.com>
2022-12-15 16:01:32 +0200

    added a query to get all the products

--- shop_me_online/src/Queries/Queries.tsx
+++ shop_me_online/src/Queries/Queries.tsx
@@ -1,2 +1,14 @@
-import React from "react"
 import { gql } from "@apollo/client"
+
+export const GET_ALL_PRODUCTS = gql`
+query Query {
+    getAllProducts {
+      id
+      name
+      quantity
+      price
+      category
+      img_location
+    }
+  }
+`;
