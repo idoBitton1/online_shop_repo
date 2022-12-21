f0257e116c9084bfd1c847fdf213eb37a0325bf2
idoBitton1 <idob5964@gmail.com>
2022-12-21 15:57:09 +0200

    removed some props and now using the products redux state

--- shop_me_online/src/Components/products/ProductsGrid.tsx
+++ shop_me_online/src/Components/products/ProductsGrid.tsx
@@ -11,19 +11,14 @@ import { ProductDisplay } from "./ProductDisplay";
 //interface
 import { Product } from "../../Pages/Home";
 
-interface MyProps {
-    products: Product[],
-    setProducts: React.Dispatch<React.SetStateAction<Product[]>>
-}
+export const ProductsGrid = () => {
 
-export const ProductsGrid: React.FC<MyProps> = ({ products, setProducts }) => {
-
-    const filtered_products = useSelector((redux_state: ReduxState) => redux_state.filtered_products);
+    const products = useSelector((redux_state: ReduxState) => redux_state.products);
 
     return (
         <div className="products_grid">
             {
-                filtered_products.map((product, i) => {
+                products.filtered_products.map((product, i) => {
 
                     return (
                         <ProductDisplay
@@ -33,9 +28,7 @@ export const ProductsGrid: React.FC<MyProps> = ({ products, setProducts }) => {
                             price={product.price}
                             quantity={product.quantity}
                             category={product.category === undefined ? "" : product.category}
-                            img_location={product.img_location}
-                            products={products}
-                            setProducts={setProducts} />
+                            img_location={product.img_location} />
                     )
                 })
             }
