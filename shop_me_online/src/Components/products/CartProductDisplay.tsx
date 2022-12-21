9514d90b05d4996807e1a3d9b2b59617d087966a
idoBitton1 <idob5964@gmail.com>
2022-12-21 14:23:46 +0200

    fetching information about the product to display some of his information

--- shop_me_online/src/Components/products/CartProductDisplay.tsx
+++ shop_me_online/src/Components/products/CartProductDisplay.tsx
@@ -1,5 +1,9 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import '../../Pages/Cart.css';
+
+//Apollo and graphql
+import { useLazyQuery } from "@apollo/client"
+import { GET_PRODUCT } from "../../Queries/Queries";
 
 //icons
 import CloseIcon from '@mui/icons-material/Close';
@@ -17,14 +21,40 @@ interface MyProps {
 
 export const CartProductDisplay: React.FC<MyProps> = ({product_id, transaction_id, address, amount, size}) => {
 
+    const [product_name, setProductName] = useState<string>("");
+    const [product_price, setProductPrice] = useState<number>(0);
+    const [product_quantity, setProductQuantity] = useState<number>(0);
+
+    const [ getProduct, { data: product_data }]  = useLazyQuery(GET_PRODUCT);
+
+    //fetch if the product_id is not null
+    useEffect(() => {
+        if(product_id){
+            getProduct({
+                variables: {
+                    id: product_id
+                }
+            })
+        }
+    }, [product_id])
+
+    //set the information in the variables to display it
+    useEffect(() => {
+        if(product_data){
+            setProductName(product_data.getProduct.name);
+            setProductPrice(product_data.getProduct.price);
+            setProductQuantity(product_data.getProduct.quantity);
+        }
+    }, [product_data])
+
     return (
         <div className="cart_product_display">
             <div className="product_info">
                 <img src={img} alt="product" className="cart_product_img" />
                 <div style={{ display: "flex", flexDirection: "column" }}>
-                    <p className="cart_product_name">Air Jordan 1 Lucky Green</p>
-                    <p>150$</p>
-                    <p>Left in stock: 149</p>
+                    <p className="cart_product_name">{product_name}</p>
+                    <p>{product_price}$</p>
+                    <p>Left in stock: {product_quantity}</p>
                 </div>
             </div>
 
