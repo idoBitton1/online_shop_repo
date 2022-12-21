d9582da2d52d0e3c3a0b0f764594bdf88f3ad240
idoBitton1 <idob5964@gmail.com>
2022-12-21 14:22:37 +0200

    fetching the information of all the cart products the user have

--- shop_me_online/src/Pages/Cart.tsx
+++ shop_me_online/src/Pages/Cart.tsx
@@ -1,19 +1,54 @@
-import React from "react";
+import React, { useEffect } from "react";
 import './Cart.css';
 
-//components
-import { Header } from "../Components/Header/Header";
+//Apollo and graphql
+import { useLazyQuery } from "@apollo/client"
+import { GET_USER_CART_PRODUCTS } from "../Queries/Queries";
 
 //redux
+import { useDispatch } from 'react-redux';
+import { actionsCreators } from "../state";
+import { bindActionCreators } from 'redux';
 import { useSelector } from 'react-redux';
 import { ReduxState } from "../state";
 
-import { Button } from "@mui/material";
+//components
+import { Header } from "../Components/Header/Header";
 import { CartProductDisplay } from "../Components/products/CartProductDisplay";
+
+//material - ui
+import { Button } from "@mui/material";
+
 
 const Cart = () => {
 
+    const user = useSelector((redux_state: ReduxState) => redux_state.user);
     const cart = useSelector((redux_state: ReduxState) => redux_state.cart);
+
+    const [ getCartProducts, { data: cart_data }]  = useLazyQuery(GET_USER_CART_PRODUCTS);
+
+    const dispatch = useDispatch();
+    const { setCart, dontFetch } = bindActionCreators(actionsCreators, dispatch);
+
+    //when the user is connecting, fetch his cart information
+    useEffect(() => {
+        if(user.fetch_info && user.token){
+            dontFetch();
+            getCartProducts({
+                variables: {
+                    userId: user.token.user_id
+                }
+            })
+        }
+    }, [user.token])
+
+    //set the information in the cart redux state
+    useEffect(() => {
+        if (cart_data) {
+            setCart(cart_data.getUserCartProducts);
+        }
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [cart_data]);
 
     return (
         <div className="cart_container">
@@ -26,15 +61,15 @@ const Cart = () => {
                     {
                         cart.map((product, i) => {
                             return (
-                                <CartProductDisplay 
-                                product_id={product.product_id}
-                                transaction_id={product.transaction_id}
-                                address={product.address}
-                                amount={product.amount}
-                                size={product.size}
-                                key={i}
+                                <CartProductDisplay
+                                    product_id={product.product_id}
+                                    transaction_id={product.transaction_id}
+                                    address={product.address}
+                                    amount={product.amount}
+                                    size={product.size}
+                                    key={i}
                                 />
-                            )
+                            );
                         })
                     }
                 </div>
