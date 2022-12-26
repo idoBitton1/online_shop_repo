9cb9ef4304794a5238961238dd95a11504f96846
idoBitton1 <idob5964@gmail.com>
2022-12-26 21:23:10 +0200

    added a mutation to delete an item from the cart

--- shop_me_online/src/Queries/Mutations.tsx
+++ shop_me_online/src/Queries/Mutations.tsx
@@ -31,6 +31,15 @@ mutation Mutation($userId: String!, $productId: String!, $size: String!, $amount
   addProductToCart(user_id: $userId, product_id: $productId, size: $size, amount: $amount, address: $address, paid: $paid, ordering_time: $orderingTime, transaction_id: $transactionId) {
     user_id
     product_id
+    quantity
+  }
+}
+`;
+
+export const DELETE_PRODUCT_FROM_CART = gql`
+mutation Mutation($transactionId: String!) {
+  deleteProductFromCart(transaction_id: $transactionId) {
+    transaction_id
   }
 }
 `;
