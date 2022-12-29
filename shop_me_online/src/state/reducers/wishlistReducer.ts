64902e00e0e4e92da06276eafdc1bab1ec7158f1
idoBitton1 <idob5964@gmail.com>
2022-12-29 16:56:34 +0200

    a new reducer for the new wishlist redux state

---
+++ shop_me_online/src/state/reducers/wishlistReducer.ts
@@ -0,0 +1,24 @@
+import { Wishlist } from "../../Pages/Home";
+import { WishlistActions } from "../actions";
+import { WishlistActionType } from "../action_types"
+
+const initial_state: Wishlist[] = [];
+
+const reducer = (state: Wishlist[] = initial_state, action: WishlistActions) => {
+    switch(action.type) {
+        case WishlistActionType.ADD_TO_WISHLIST:
+            return [...state, action.payload];
+        case WishlistActionType.REMOVE_FROM_WISHLIST:
+            {
+                let index_of_current_product = -1;
+
+                index_of_current_product = state.findIndex((product) => product.user_id === action.payload.user_id && product.product_id === action.payload.product_id);
+
+                return [...state.slice(0, index_of_current_product), ...state.slice(index_of_current_product + 1)];
+            }
+        default:
+            return state;
+    }
+}
+
+export default reducer;
