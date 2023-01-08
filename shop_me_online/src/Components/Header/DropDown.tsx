0671b7b7d0a21d521ecc6115967c67ecd994c822
idoBitton1 <idob5964@gmail.com>
2023-01-08 13:58:39 +0200

    changed the name of the copmponent

---
+++ shop_me_online/src/Components/Header/DropDown.tsx
@@ -0,0 +1,54 @@
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+//redux
+import { useSelector } from 'react-redux';
+import { ReduxState } from "../../state";
+
+//components
+import { ConnectedUserDD } from "./ConnectedUserDD";
+
+//icons
+import MenuIcon from '@mui/icons-material/Menu';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import { AiOutlineHome } from 'react-icons/ai';
+import { DisconnectedUserDD } from "./DisconnectedUserDD";
+
+export const DropDown = () => {
+
+  const user = useSelector((redux_state: ReduxState) => redux_state.user);
+
+  const navigate = useNavigate();
+  const [open, setOpen] = useState<boolean>(false);
+
+  const toggleDropDown = () => {
+    setOpen((prev) => !prev)
+  }
+
+  return (
+    <>
+      <button className="button dropdown_btn" onClick={toggleDropDown}>
+        <MenuIcon sx={{ fontSize: 40 }} />
+        <AccountCircleIcon sx={{ fontSize: 40 }} />
+      </button>
+
+      <div className={`dropdown_menu ${open ? 'active' : 'inactive'}`} >
+        <ul>
+          <li className='dropdown_item' onClick={() => navigate('/')}>
+            <AiOutlineHome className="dropdown_item_icon" />
+            <h3> home </h3>
+          </li>
+          {
+            user.token
+              ?
+              //things that connected users see
+              <ConnectedUserDD toggleDropDown={toggleDropDown} />
+              :
+              //things that unconnected users see
+              <DisconnectedUserDD />
+          }
+        </ul>
+      </div>
+    </>
+  )
+}
