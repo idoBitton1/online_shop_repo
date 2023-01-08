7573644fe02b5e396471967f715cb76157c0fa3c
idoBitton1 <idob5964@gmail.com>
2023-01-08 13:59:00 +0200

    the profile page

---
+++ shop_me_online/src/Pages/Profile.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+//components
+import { Header } from "../Components/Header/Header";
+
+const Profile = () => {
+
+    return (
+        <div className="profile_container">
+            <Header />
+
+            <div className="profile_form">
+                
+            </div>
+        </div>
+    );
+}
+
+export default Profile;
