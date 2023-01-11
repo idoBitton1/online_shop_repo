0ceb01c168e071e5524c0e1f7b7ca9495b275ad8
idoBitton1 <idob5964@gmail.com>
2023-01-11 19:45:45 +0200

    added a remove credit card option, and add a credit card option

--- shop_me_online/src/Components/Forms/ProfileForm.tsx
+++ shop_me_online/src/Components/Forms/ProfileForm.tsx
@@ -6,31 +6,53 @@ import { Formik, Form, Field, ErrorMessage } from "formik";
 
 //Apollo and graphql
 import { useQuery, useMutation } from "@apollo/client"
-import { GET_USER } from "../../Queries/Queries";
-import { UPDATE_USER_INFORMATION } from "../../Queries/Mutations";
+import { GET_USER, CHECK_FOR_CREDIT_CARD } from "../../Queries/Queries";
+import { UPDATE_USER_INFORMATION, REMOVE_CREDIT_CARD } from "../../Queries/Mutations";
 
 //redux
 import { useSelector } from 'react-redux';
 import { ReduxState } from "../../state";
 
+//components
+import { CreditCardForm } from "./CreditCardForm";
+
 //material-ui
-import { TextField, Button, Typography } from '@mui/material';
+import { TextField, Button, Typography, InputAdornment } from '@mui/material';
+import Dialog from '@mui/material/Dialog';
+import DialogContent from '@mui/material/DialogContent';
+import DialogTitle from '@mui/material/DialogTitle';
+
+//icons
+import PermIdentityOutlinedIcon from '@mui/icons-material/PermIdentityOutlined';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import EmailOutlinedIcon from '@mui/icons-material/EmailOutlined';
+import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined';
+import AddCircleOutlineOutlinedIcon from '@mui/icons-material/AddCircleOutlineOutlined';
+import RemoveCircleOutlineOutlinedIcon from '@mui/icons-material/RemoveCircleOutlineOutlined';
 
 //interface
 import { MyFormValues } from "./RegisterForm";
 
 export const ProfileForm = () => {
-
     const user = useSelector((redux_state: ReduxState) => redux_state.user);
+    
+    const [has_credit_card, setHasCreditCard] = useState<boolean>(false);
     const [is_manager, setIsManager] = useState<boolean>(false);
+    const [open, setOpen] = useState<boolean>(false);
 
     const { data: user_data } = useQuery(GET_USER, {
         variables: {
             userId: user.token?.user_id
         }
     });
+    const { data: credit_data } = useQuery(CHECK_FOR_CREDIT_CARD, {
+        variables: {
+            id: user.token?.user_id
+        }
+    });
 
     const [updateUserInformation, { error }] = useMutation(UPDATE_USER_INFORMATION);
+    const [removeCreditCard] = useMutation(REMOVE_CREDIT_CARD);
 
     //the initial values of the form to the user information
     const initial_values: MyFormValues ={
@@ -77,15 +99,40 @@ export const ProfileForm = () => {
         });
     }
 
-    const handleCreditCartClick = () => {
-        
+    useEffect(() => {
+        if(credit_data) {
+            setHasCreditCard(credit_data.checkForCreditCard);
+        }
+    }, [credit_data]);
+
+    const handleCreditCardClick = () => {
+        toggleDialog();
+    }
+
+    const handleRemoveCreditCardClick = () => {
+        //set to dont have credit card
+        setHasCreditCard(false);
+
+        //remove from the db
+        removeCreditCard({
+            variables: {
+                id: user.token?.user_id
+            }
+        });
+
+        window.location.reload(); //refresh the window
+    }
+
+    const toggleDialog = () => {
+        setOpen((prev) => !prev);
     }
 
     return (
+        <>
         <Formik
-            initialValues={initial_values}
-            validationSchema={validation_schema}
-            onSubmit={onSubmit}
+        initialValues={initial_values}
+        validationSchema={validation_schema}
+        onSubmit={onSubmit}
         >
             {(props) => (
                 <Form>
@@ -97,6 +144,13 @@ export const ProfileForm = () => {
                         margin="normal"
                         sx={{ marginRight: 1 }}
                         helperText={<ErrorMessage name="first_name" />}
+                        InputProps={{
+                            startAdornment: (
+                              <InputAdornment position="start">
+                                <PermIdentityOutlinedIcon />
+                              </InputAdornment>
+                            )
+                        }}
                     />
                     <Field as={TextField} name="last_name"
                         label="last name"
@@ -105,6 +159,13 @@ export const ProfileForm = () => {
                         onChange={props.handleChange}
                         margin="normal"
                         helperText={<ErrorMessage name="last_name" />}
+                        InputProps={{
+                            startAdornment: (
+                              <InputAdornment position="start">
+                                <PermIdentityOutlinedIcon />
+                              </InputAdornment>
+                            )
+                        }}
                     />
 
                     <br />
@@ -117,6 +178,13 @@ export const ProfileForm = () => {
                         margin="normal"
                         fullWidth
                         helperText={<ErrorMessage name="password" />}
+                        InputProps={{
+                            startAdornment: (
+                              <InputAdornment position="start">
+                                <LockOutlinedIcon />
+                              </InputAdornment>
+                            )
+                        }}
                     />
 
                     <br />
@@ -129,6 +197,13 @@ export const ProfileForm = () => {
                         margin="normal"
                         fullWidth
                         helperText={<ErrorMessage name="email" />}
+                        InputProps={{
+                            startAdornment: (
+                              <InputAdornment position="start">
+                                <EmailOutlinedIcon />
+                              </InputAdornment>
+                            )
+                        }}
                     />
 
                     <br />
@@ -146,22 +221,41 @@ export const ProfileForm = () => {
                                     margin="normal"
                                     fullWidth
                                     helperText={<ErrorMessage name="address" />}
+                                    InputProps={{
+                                        startAdornment: (
+                                          <InputAdornment position="start">
+                                            <HomeOutlinedIcon />
+                                          </InputAdornment>
+                                        )
+                                    }}
                                 />
                             )
                     }
 
                     {is_manager ? <></> : <br />}
+
                     <br />
-                    <h3 className="point_me" onClick={handleCreditCartClick}
-                        style={{ color: "gray", textAlign: "left" }}
-                        >
-                            add credit card
+                    {
+                    has_credit_card
+                    ?
+                    <h3 className="point_me" onClick={handleRemoveCreditCardClick}
+                    style={{ textAlign: "left", display: "flex", alignItems: "center" }}
+                    >
+                        <RemoveCircleOutlineOutlinedIcon /> &nbsp; remove credit card
                     </h3>
+                    :
+                    <h3 className="point_me" onClick={handleCreditCardClick}
+                    style={{ textAlign: "left", display: "flex", alignItems: "center" }}
+                    >
+                        <AddCircleOutlineOutlinedIcon /> &nbsp; add credit card
+                    </h3>
+                    }
+
                     <br />
                     <h3
-                        style={{ fontFamily: "Rubik" ,color: "GrayText", textAlign: "left" }}
-                        >
-                            *if no data is displaying, go to another page and come back
+                    style={{ fontFamily: "Rubik" ,color: "GrayText", textAlign: "left" }}
+                    >
+                        *if no data is displaying, go to another page and come back
                     </h3>
 
                     <br />
@@ -181,5 +275,28 @@ export const ProfileForm = () => {
                 </Form>
             )}
         </Formik>
+
+
+
+
+        <Dialog open={open} onClose={toggleDialog} fullWidth>
+            <DialogTitle>
+                <Typography
+                fontSize={25}
+                borderBottom={1}
+                borderColor={"lightgray"}
+                gutterBottom>
+                    Add credit card
+                </Typography>
+            </DialogTitle>
+
+            <DialogContent>
+                <CreditCardForm 
+                toggleDialog={toggleDialog}
+                setHasCreditCard={setHasCreditCard}
+                />
+            </DialogContent>
+        </Dialog>
+        </>
     );
 }
