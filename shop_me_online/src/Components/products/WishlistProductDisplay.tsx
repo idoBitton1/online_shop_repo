6456ae44758feda0d603817d12f3bcb562ab7a53
idoBitton1 <idob5964@gmail.com>
2023-01-15 16:57:43 +0200

    can now order a product that is in your wishlist

--- shop_me_online/src/Components/products/WishlistProductDisplay.tsx
+++ shop_me_online/src/Components/products/WishlistProductDisplay.tsx
@@ -18,6 +18,11 @@ import CloseIcon from '@mui/icons-material/Close';
 
 //images
 import img from "../../Images/j1.png";
+import { OrderProduct } from "./OrderProduct";
+
+//interface
+import { Product } from "../../Pages/Home";
+import { Button } from "@mui/material";
 
 interface MyProps {
     user_id: string,
@@ -25,18 +30,30 @@ interface MyProps {
 }
 
 export const WishlistProductDisplay: React.FC<MyProps> = ({user_id, product_id}) => {
-
+    //redux states
     const products = useSelector((redux_state: ReduxState) => redux_state.products);
 
     const dispatch = useDispatch();
     const { removeFromWishlist } = bindActionCreators(actionsCreators, dispatch);
 
-    const [product_name, setProductName] = useState<string>("");
-    const [product_price, setProductPrice] = useState<number>(0);
-    const [product_quantity, setProductQuantity] = useState<number>(0);
+    //states
+    const [open_dialog, SetOpenDialog] = useState<boolean>(false);
+    const [product_info, setProductInfo] = useState<Product>({
+        id: product_id,
+        name: "",
+        price: 0,
+        quantity: 0,
+        category: "",
+        img_location: ""
+    });
+    // const [product_name, setProductName] = useState<string>("");
+    // const [product_price, setProductPrice] = useState<number>(0);
+    // const [product_quantity, setProductQuantity] = useState<number>(0);
 
+    //queries
     const [ getProduct, { data: product_data }]  = useLazyQuery(GET_PRODUCT);
 
+    //mutations
     const [deleteProductFromWishlist] = useMutation(DELETE_PRODUCT_FROM_WISHLIST);
 
     //fetch if the product_id is not null
@@ -53,17 +70,23 @@ export const WishlistProductDisplay: React.FC<MyProps> = ({user_id, product_id})
 
     //set the information in the variables to display it
     useEffect(() => {
-        if(product_data){
-            setProductName(product_data.getProduct.name);
-
-            setProductPrice(product_data.getProduct.price);
+        if(product_data) {
+            setProductInfo((prev) => {
+                return {
+                    id: prev.id,
+                    name: product_data.getProduct.name,
+                    price: product_data.getProduct.price,
+                    quantity: product_data.getProduct.quantity,
+                    category: product_data.getProduct.category,
+                    img_location: product_data.getProduct.img_location
+                }
+            })
 
             if(products.products.length !== 0) {
                 let index = products.products.findIndex((product) => product.id === product_id);
-                setProductQuantity(products.products[index].quantity); //a more accurate quantity
+                //a more accurate quantity
+                setProductInfo((prev) => ({...prev, quantity: products.products[index].quantity}));
             }
-            else
-                setProductQuantity(product_data.getProduct.quantity);// plan B
         }
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [product_data]);
@@ -82,15 +105,26 @@ export const WishlistProductDisplay: React.FC<MyProps> = ({user_id, product_id})
             product_id: product_id
         });
     }
+
+    const toggleDialog = () => {
+        SetOpenDialog((prev) => !prev);
+    }
     
     return (
+        <>
         <div className="wishlist_product_display">
             <div className="product_info">
                 <img src={img} alt="product" className="cart_product_img" />
-                <div style={{ display: "flex", flexDirection: "column" }}>
-                    <p className="cart_product_name">{product_name}</p>
-                    <p>price for each: {product_price}$</p>
-                    <p>Left in stock: {product_quantity}</p>
+                <div style={{ display: "flex", flexDirection: "column",  }}>
+                    <p className="cart_product_name">{product_info.name}</p>
+                    <p>price for each: {product_info.price}$</p>
+                    <p>Left in stock: {product_info.quantity}</p>
+                    
+                    <Button variant="contained"
+                    onClick={toggleDialog}
+                    >
+                        Buy now
+                    </Button>
                 </div>
             </div>
 
@@ -100,5 +134,17 @@ export const WishlistProductDisplay: React.FC<MyProps> = ({user_id, product_id})
                 </button>
             </div>
         </div>
+
+        <OrderProduct 
+        is_open={open_dialog}
+        toggleDialog={toggleDialog}
+        id={product_id}
+        name={product_info.name}
+        price={product_info.price}
+        quantity={product_info.quantity}
+        category={product_info.category}
+        img_location={product_info.img_location}
+        />
+        </>
     )
 }
