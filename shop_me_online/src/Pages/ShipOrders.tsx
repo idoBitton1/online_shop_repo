b6067f7e45c0459ddced776e6a81f56eaa0af370
idoBitton1 <idob5964@gmail.com>
2023-01-18 18:42:31 +0200

    the ship orders page

---
+++ shop_me_online/src/Pages/ShipOrders.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Header } from "../Components/Header/Header";
+
+const ShipOrders = () => {
+
+    return (
+        <div className="ship_orders_container">
+            <Header />
+
+            ship
+        </div>
+    );
+}
+
+export default ShipOrders;
