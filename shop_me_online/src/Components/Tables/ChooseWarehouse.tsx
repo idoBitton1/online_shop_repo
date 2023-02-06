a2e51b6dba4d9c9c71e4e0d8388b1c98b6fccf22
idoBitton1 <idob5964@gmail.com>
2023-02-06 12:38:20 +0200

    created 2 new tables: display and select which transactions to ship, and the second is to select from which warehouse(s) to ship from

---
+++ shop_me_online/src/Components/Tables/ChooseWarehouse.tsx
@@ -0,0 +1,57 @@
+import React from "react";
+import '../../Pages/ShipOrders.css';
+
+//material ui
+import { DataGrid, GridColDef } from '@mui/x-data-grid';
+
+//interface
+import { Warehouse } from "../../Pages/ShipOrders";
+
+interface MyProps {
+    setSelectedWarehouses: React.Dispatch<React.SetStateAction<Warehouse[]>>
+}
+
+const columns: GridColDef[] = [
+    { field: 'id', headerName: 'ID', width: 50 },
+    { field: 'name', headerName: 'Name', width: 150 },
+    { field: 'address', headerName: 'Address', width: 130 }
+];
+
+const rows = [
+    { id: 1, name: "First warehouse", address: "Tel-Aviv" },
+    { id: 2, name: "Second warehouse", address: "Heifa" },
+    { id: 3, name: "Third warehouse", address: "Beer-Sheva" }
+  ];
+
+export const ChooseWarehouse: React.FC<MyProps> = ({setSelectedWarehouses}) => {
+
+    return (
+        <div className="warehouse_table_container">
+            <p className="secondary_headlines">Select a warehouse to ship from:</p>
+
+            <div style={{ height: 267, width: 400, margin: 'auto' }}>
+            <DataGrid
+            rows={rows}
+            columns={columns}
+            pageSize={3}
+            rowsPerPageOptions={[3]}
+            checkboxSelection
+            onSelectionModelChange={(ids) => {
+                const selectedIDs = new Set(ids);
+                //filter the array to get only the selected rows, and take only the address field
+                const selectedRowsData = rows.filter((row) =>
+                    selectedIDs.has(row.id)
+                ).map((row) => {
+                    return {
+                        name: row.name,
+                        address: row.address
+                    };
+                });
+
+                setSelectedWarehouses(selectedRowsData);
+            }}
+            />
+            </div>
+        </div>
+    );
+}
---
+++ shop_me_online/src/Components/Tables/TransactionsTable.tsx
@@ -0,0 +1,78 @@
+import React, { useState } from "react";
+import '../../Pages/ShipOrders.css';
+
+//apollo and graphql
+import { useQuery } from "@apollo/client";
+import { GET_TRANSACTIONS } from "../../Queries/Queries";
+
+//material ui
+import { DataGrid, GridColDef } from '@mui/x-data-grid';
+
+//interface
+import { Transaction } from "../../Pages/Home";
+
+interface MyProps {
+    setSelectedTransactions: React.Dispatch<React.SetStateAction<Transaction[]>>
+}
+
+const columns: GridColDef[] = [
+    { field: 'id', headerName: 'ID', width: 300 },
+    { field: 'address', headerName: 'Address', width: 130 },
+    {
+      field: 'paid',
+      headerName: 'Paid',
+      type: 'boolean',
+      width: 90
+    },
+    {
+      field: 'ordering_time',
+      headerName: 'Ordering time',
+      width: 110
+    }
+];
+
+export const TransactionsTable: React.FC<MyProps> = ({setSelectedTransactions}) => {
+    //states
+    const [transactions, setTransactions] = useState<Transaction[]>([]);
+    
+    //queries
+    useQuery(GET_TRANSACTIONS, {
+        onCompleted(data) {
+            let temp: Transaction[] = data.getTransactions;
+            temp = temp.map((item) => {
+                let fixed_time = (Number)(item.ordering_time.slice(0, -3));
+                const timestamp_to_date = new Date(fixed_time * 1000);
+                return {
+                    ...item,
+                    ordering_time: `${timestamp_to_date.getFullYear()}-${(timestamp_to_date.getMonth() + 1) >= 10 ? timestamp_to_date.getMonth() + 1 : `0${timestamp_to_date.getMonth() + 1}`}-${timestamp_to_date.getDate() >= 10 ? timestamp_to_date.getDate() : `0${timestamp_to_date.getDate()}`}`
+                }
+            });
+
+            setTransactions([...temp]);
+        }
+    });
+
+    return (
+        <div className="transaction_table_container">
+            <p className="secondary_headlines">Select transactions to ship:</p>
+            
+            <div style={{ height: 400, width: 700, margin: 'auto' }}>
+            <DataGrid
+            rows={transactions}
+            columns={columns}
+            pageSize={5}
+            rowsPerPageOptions={[5]}
+            checkboxSelection
+            onSelectionModelChange={(ids) => {
+                const selectedIDs = new Set(ids);
+                const selectedRowsData = transactions.filter((row) =>
+                    selectedIDs.has(row.id)
+                );
+
+                setSelectedTransactions(selectedRowsData);
+            }}
+            />
+            </div>
+        </div>
+    );
+}
