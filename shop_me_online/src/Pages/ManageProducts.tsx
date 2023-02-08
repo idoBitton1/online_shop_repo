1077e2bf468bfd1afc95ea11b93a2f9e988d78c1
idoBitton1 <idob5964@gmail.com>
2023-02-08 15:53:53 +0200

    a page for the manager to manage all the products

---
+++ shop_me_online/src/Pages/ManageProducts.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const ManageProducts = () => {
+
+    return (
+        <div className="manage_products_container">
+            
+        </div>
+    );
+}
+
+export default ManageProducts;
