90f021877d7bfb93d2f617968522e0c1b504478e
unknown <idob5964@gmail.com>
2023-02-16 16:59:53 +0200

    the manage products page

--- shop_me_online/src/Pages/ManageProducts.tsx
+++ shop_me_online/src/Pages/ManageProducts.tsx
@@ -1,10 +1,19 @@
 import React from "react";
+import './ManageProducts.css';
+
+import { Header } from "../Components/Header/Header";
 
 const ManageProducts = () => {
 
     return (
         <div className="manage_products_container">
-            
+            <Header />
+
+            <h1 className="headline"> Manage products </h1>
+
+            <div className="manage_products_context">
+                
+            </div>
         </div>
     );
 }
