7066944c657f8f863a0ebac2be2241bfd4287205
unknown <idob5964@gmail.com>
2023-02-16 19:37:50 +0200

    added the navigation bar and products grid components, and added a new Add product button. displaying all the product because you can edit all of them

--- shop_me_online/src/Pages/ManageProducts.tsx
+++ shop_me_online/src/Pages/ManageProducts.tsx
@@ -1,18 +1,41 @@
 import React from "react";
 import './ManageProducts.css';
 
+//components
 import { Header } from "../Components/Header/Header";
+import { ProductsGrid } from "../Components/products/ProductsGrid";
+import { NavigationBar } from "../Components/Header/NavigationBar";
+
+//redux
+import { useSelector } from 'react-redux';
+import { ReduxState } from "../state";
 
 const ManageProducts = () => {
+    //redux states
+    const products = useSelector((redux_state: ReduxState) => redux_state.products);
+
+    const handleAddProductClick = () => {
+        
+    }
 
     return (
         <div className="manage_products_container">
             <Header />
 
-            <h1 className="headline"> Manage products </h1>
+            <div className="add_button_container">
+                <button className="add_button">
+                    Add Product
+                </button>
+            </div> 
+
+            <h1 className="headline"> Manage products </h1>                
 
             <div className="manage_products_context">
-                
+                <NavigationBar
+                    products={products.products}
+                />
+
+                <ProductsGrid to_manage_product={true} />
             </div>
         </div>
     );
