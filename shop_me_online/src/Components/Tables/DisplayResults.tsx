aac7a1964a6ccd50fd208084f646eece09c0a738
idoBitton1 <idob5964@gmail.com>
2023-03-13 13:49:47 +0200

    changed the rows to column and columns to rows in the table

--- shop_me_online/src/Components/Tables/DisplayResults.tsx
+++ shop_me_online/src/Components/Tables/DisplayResults.tsx
@@ -1,34 +1,79 @@
-import React from "react";
+import React, { useState } from "react";
 import '../../Pages/ShipOrders.css';
+
+//Apollo and graphql
+import { useMutation } from "@apollo/client";
+import { DELETE_TRANSACTION } from "../../Queries/Mutations";
+
+//components
+import { DisplayCell } from "./DisplayCell";
+import { DisplayColumn } from "./DisplayColumn";
 
 //interface
 import { Result, Warehouse } from "../../Pages/ShipOrders";
-import { DisplayCell } from "./DisplayCell";
-import { DisplayRow } from "./DisplayRow";
+import { TransactionSecondType } from "../../Pages/Home";
+import { Button } from "@mui/material";
 
 interface MyProps extends Result {
-    selected_transactions_addresses: string[],
+    selected_transactions: TransactionSecondType[],
     selected_warehouses: Warehouse[]
 }
 
-export const DisplayResults: React.FC<MyProps> = ({result_matrix, total_cost, selected_transactions_addresses, selected_warehouses}) => {
+export const DisplayResults: React.FC<MyProps> = ({result_matrix, total_cost, selected_transactions, selected_warehouses}) => {
+    //states
+    const [reduced_transactions, setReducedTransactions] = useState<TransactionSecondType[]>(
+        selected_transactions.filter((transaction, i) => {
+            return selected_transactions.indexOf(transaction) === i;
+        })
+    );
+
+    //mutations
+    const [deleteTransaction] = useMutation(DELETE_TRANSACTION);
+
+
+    //delete the selected transactions 
+    const handleConfirm = () => {
+        selected_transactions.map((transaction) => {
+            deleteTransaction({
+                variables: {
+                    transaction_id: transaction.id
+                }
+            });
+
+            return transaction;
+        });
+
+        window.location.reload(); //refresh
+    }
 
     return (
+        <>
         <div className="results_container">
             <div className="result_table">
                 <div className="display_flex">
                     <DisplayCell text={""} />
-                    {selected_transactions_addresses.map((address, i) => <DisplayCell text={address} key={i} />)}
+                    {selected_warehouses.map((warehouse, i) => <DisplayCell text={warehouse.name} key={i} />)}
                 </div>
 
-                {
-                    selected_warehouses.map((item, i) => {
-                        return <DisplayRow row_name={item.name} row_values={result_matrix[i]} key={i} />
-                    })
-                }
+                <div className="display_flex">
+                    <DisplayColumn column_values={reduced_transactions.map((transaction) => transaction.address)} />             
+                    {
+                        result_matrix.map((value, i) => {
+                            return <DisplayColumn column_values={value} key={i} />
+                        })
+                    }
+                </div>
             </div>
 
             <h2 className="total_cost_text">total cost: {total_cost}</h2>
         </div>
+        <div className="confirm_button">
+            <Button
+            variant="contained"
+            onClick={handleConfirm}>
+                Confirm
+            </Button>
+        </div>
+        </>
     );
 }
